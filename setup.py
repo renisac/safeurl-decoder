7b819812f96630d67d1ec8009434356575ab2cad
Michael Davis <mdavis332@gmail.com>
2020-06-18 10:13:14 -0500

    bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='safeurl_decoder',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     url='https://github.com/renisac/safeurl_decoder',
     author='REN-ISAC',
-    author_email='soc@ren-isac.netß',
+    author_email='soc@ren-isac.net',
     description='',
     entry_points = {
               'console_scripts': ['safeurl-decode=safeurl_decoder.__init__:main'],
